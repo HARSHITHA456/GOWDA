5032d348768d4d0592d32b1caa6bbde1463d9085
VIBHASREE <35587745+VIBHASREE@users.noreply.github.com>
2018-01-19 10:41:16 +0530

    Add files via upload

---
+++ Program1.py
@@ -0,0 +1,87 @@
+Python 3.6.3 (default, Oct  3 2017, 21:45:48) 
+[GCC 7.2.0] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> print("VIBHASREE")
+VIBHASREE
+>>> 
+============== RESTART: /home/ec2017b221/vibhasree/program1.py ==============
+VIBHASREE
+>>> 
+============== RESTART: /home/ec2017b221/vibhasree/program1.py ==============
+VIBHASREE
+i am studing in cmru
+>>> 
+============== RESTART: /home/ec2017b221/vibhasree/program1.py ==============
+VIBHASREE
+i am studing in cmru
+i am an engineering student
+>>> 
+============== RESTART: /home/ec2017b221/vibhasree/program1.py ==============
+Traceback (most recent call last):
+  File "/home/ec2017b221/vibhasree/program1.py", line 2, in <module>
+    print["VIBHASREE"]
+TypeError: 'builtin_function_or_method' object is not subscriptable
+>>> 500+300=
+SyntaxError: invalid syntax
+>>> SyntaxError: invalid synta
+SyntaxError: invalid syntax
+>>> 500+400
+900
+>>> 400+900
+
+1300
+>>> 500-400
+
+100
+>>> 2000000*400
+
+800000000
+>>> 365400/32450
+
+11.26040061633282
+>>> 800034/328650
+
+2.434303970789594
+>>> 
+============== RESTART: /home/ec2017b221/vibhasree/program1.py ==============
+Traceback (most recent call last):
+  File "/home/ec2017b221/vibhasree/program1.py", line 2, in <module>
+    print["VIBHASREE"]
+TypeError: 'builtin_function_or_method' object is not subscriptable
+>>> 
+============== RESTART: /home/ec2017b221/vibhasree/program1.py ==============
+Traceback (most recent call last):
+  File "/home/ec2017b221/vibhasree/program1.py", line 2, in <module>
+    print["VIBHASREE"]
+TypeError: 'builtin_function_or_method' object is not subscriptable
+>>> 
+============== RESTART: /home/ec2017b221/vibhasree/program1.py ==============
+Traceback (most recent call last):
+  File "/home/ec2017b221/vibhasree/program1.py", line 2, in <module>
+    print["VIBHASREE"]
+TypeError: 'builtin_function_or_method' object is not subscriptable
+>>> 2%11
+
+2
+>>> 11%5
+
+1
+>>> 
+============== RESTART: /home/ec2017b221/vibhasree/program2.py ==============
+enter the value of x and y
+>>> 
+============== RESTART: /home/ec2017b221/vibhasree/program2.py ==============
+enter the value of x and y
+enter a value
+============== RESTART: /home/ec2017b221/vibhasree/program2.py ==============
+enter the value of x and y
+200
+============== RESTART: /home/ec2017b221/vibhasree/program2.py ==============
+enter the value of x and y
+200
+============== RESTART: /home/ec2017b221/vibhasree/program2.py ==============
+enter the value of a and b
+200
+============== RESTART: /home/ec2017b221/vibhasree/program2.py ==============
+enter the value of a and b
+enter a value200
