3738634a0d0d2556929c2657a00a7b500193a700
VIBHASREE <35587745+VIBHASREE@users.noreply.github.com>
2018-02-02 08:53:10 +0530

    Add files via upload

---
+++ prog.snake.py
@@ -0,0 +1,117 @@
+Python 3.6.3 (default, Oct  3 2017, 21:45:48) 
+[GCC 7.2.0] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+=============== RESTART: /home/ec2017b221/prog4input.snake.py ===============
+pressr to roll a die55
+CONTINUE THE GAME
+pressr to roll a dies
+your random is 1
+your count is 1
+CONTINUE THE GAME
+pressr to roll a dies
+your random is 5
+your count is 6
+CONTINUE THE GAME
+pressr to roll a dies
+your random is 1
+your count is 7
+CONTINUE THE GAME
+pressr to roll a dies
+your random is 2
+your count is 9
+CONTINUE THE GAME
+pressr to roll a dies
+your random is 6
+your count is 15
+CONTINUE THE GAME
+pressr to roll a dies
+your random is 2
+your count is 17
+CONTINUE THE GAME
+pressr to roll a dies
+your random is 6
+your count is 23
+CONTINUE THE GAME
+pressr to roll a dies
+your random is 3
+your count is 26
+CONTINUE THE GAME
+pressr to roll a dies
+your random is 1
+your count is 27
+CONTINUE THE GAME
+pressr to roll a dies
+your random is 3
+your count is 30
+CONTINUE THE GAME
+pressr to roll a dies
+your random is 3
+your count is 33
+CONTINUE THE GAME
+pressr to roll a dies
+your random is 4
+your count is 37
+CONTINUE THE GAME
+pressr to roll a dies
+your random is 6
+your count is 43
+CONTINUE THE GAME
+pressr to roll a dies
+your random is 2
+your count is 45
+CONTINUE THE GAME
+pressr to roll a dies
+your random is 4
+your count is 49
+CONTINUE THE GAME
+pressr to roll a dies
+your random is 6
+your count is 55
+CONTINUE THE GAME
+pressr to roll a dies
+your random is 4
+your count is 59
+CONTINUE THE GAME
+pressr to roll a dies
+your random is 4
+your count is 63
+CONTINUE THE GAME
+pressr to roll a dies
+your random is 2
+your count is 65
+ohh snake bit come down and count is 46
+pressr to roll a dies
+your random is 6
+your count is 52
+wow climbed up the ladder and count is 81
+CONTINUE THE GAME
+pressr to roll a dies
+your random is 3
+your count is 84
+CONTINUE THE GAME
+pressr to roll a dies
+your random is 4
+your count is 88
+CONTINUE THE GAME
+pressr to roll a dies
+your random is 4
+your count is 92
+CONTINUE THE GAME
+pressr to roll a dies
+your random is 3
+your count is 95
+CONTINUE THE GAME
+pressr to roll a dies
+your random is 3
+your count is 98
+CONTINUE THE GAME
+pressr to roll a dies
+your random is 2
+your count is 100
+win
+pressr to roll a dies
+your random is 5
+your count is 105
+win
+>>> 
